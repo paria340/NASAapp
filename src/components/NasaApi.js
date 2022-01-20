585ea6d3fa01dc88ffd488c6c48c18cf2256237e
Parisa Moshfegh <paria340@gmail.com>
2022-01-19 16:26:19 -0800

    like

--- src/components/NasaApi.js
+++ src/components/NasaApi.js
@@ -3,15 +3,18 @@ import { useEffect, useState } from 'react'
 
 function NasaApi() {
     const [data, setData] = useState([])
-    const [like, setLike] = useState(false)
+    const [like, setLike] = useState([false, false, false, false, false, false, false, false, false, false])
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         handleQuery()
-    }, [])
+        
+      }, [])
 
     const handleQuery = () => {
         const apiKey = 'PKW8W4L6a7m3FJOQSruIS7RJsyTEstuuW32tmYGD'
         const apiURL = 'https://api.nasa.gov/planetary/apod'
+        setLoading(true)
         axios({
             url: apiURL,
             method: 'GET',
@@ -22,26 +25,34 @@ function NasaApi() {
             }
         }).then(response => {
             setData(response.data)
+            setLoading(false)
         })
     }
 
-    const handleLike = () => {
-        !like ? setLike(true) : setLike(false)
+    if (loading) {
+        return (
+          <span className='loading'>Loading Wait Please...</span>
+        )
+    }
+
+    const handleLike = (index) => {
+        setLike(prevState => prevState.map((item, idx) => idx === index ? !item : item))
     }
 
     return(
         <section>
             {
-                data.map(indivData => {
+                data.map((indivData, index) => {
+                    
                     return(
                         <div className='dataDisplay' key={indivData.date}>
                             <img src={indivData.url} alt={indivData.explanation} />
                             <h2>{indivData.title}</h2>
                             <h3>{indivData.date}</h3>
-                            <button onClick={() => handleLike()}>
+                            <button onClick={() => handleLike(index)}> 
                                 {
-                                    like ?
-                                    <p>Liked</p>
+                                    like[index] ?
+                                    <p>Liked</p> 
                                     : <p>Like</p>
                                 }                            
                             </button>
